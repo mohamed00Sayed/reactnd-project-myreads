34ceff373589c2ae4a3d675b63da0dc7e94c8f96
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-07 14:14:10 +0200

    refactor: Refactoring (ShelfChanger) component.

--- src/main/ShelfChanger.js
+++ src/main/ShelfChanger.js
@@ -4,7 +4,15 @@ class ShelfChanger extends Component{
 	
 	render(){
 		return(
-			<div>ShelfChanger Component</div>
+			<div className="book-shelf-changer">
+				<select>
+					<option value="move" disabled>Move to...</option>
+					<option value="currentlyReading">Currently Reading</option>
+					<option value="wantToRead">Want to Read</option>
+					<option value="read">Read</option>
+					<option value="none">None</option>
+				</select>
+			</div>
 		)
 	}
 }
