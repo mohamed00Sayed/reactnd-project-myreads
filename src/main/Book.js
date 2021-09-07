0d43df21a11c6e4f7bacbac4e5de75f5b9225979
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-07 14:09:32 +0200

    feat: Adding (ShelfChanger) component to (Book) component.

--- src/main/Book.js
+++ src/main/Book.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import ShelfChanger from './ShelfChanger'
 
 class Book extends Component{
 	
 	render(){
 		return(
-			<div>Book Component</div>
+			<div>
+				<h6>Book Component</h6>
+				<ShelfChanger />
+			</div>
 		)
 	}
 }
---
+++ src/main/ShelfChanger.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class ShelfChanger extends Component{
+	
+	render(){
+		return(
+			<div>ShelfChanger Component</div>
+		)
+	}
+}
+
+export default ShelfChanger
