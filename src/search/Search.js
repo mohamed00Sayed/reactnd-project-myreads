2e1480be97561f160076e59ce0eeabc7674ce95a
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-07 13:44:14 +0200

    feat: Adding (SearchBar) component to (Search) component.

--- src/search/Search.js
+++ src/search/Search.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import SearchBar from './SearchBar'
 
 class Search extends Component{
 	
 	render(){
 		return(
-			<div>Search Component</div>
+			<div>
+				<h3>Search Component</h3>
+				<SearchBar />
+			</div>
 		)
 	}
 }
---
+++ src/search/SearchBar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class SearchBar extends Component{
+	
+	render(){
+		return(
+			<div>SearchBar Component</div>
+		)
+	}
+}
+
+export default SearchBar
