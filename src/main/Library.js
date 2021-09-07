a405ca8b75cc8ddc632c6fa2d8246052d24b7a08
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-07 13:12:37 +0200

    feat: Adding (Shelf) components to (Library) component.

--- src/main/Library.js
+++ src/main/Library.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import Shelf from './Shelf'
 
 class Library extends Component{
 	
 	render(){
 		return(
-			<div>Library Component</div>
+			<div>
+				<h4>Library Component</h4>
+				<Shelf />
+				<Shelf />
+				<Shelf />
+			</div>
 		)
 	}
 }
---
+++ src/main/Shelf.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Shelf extends Component{
+	
+	render(){
+		return(
+			<div>Shelf Component</div>
+		)
+	}
+}
+
+export default Shelf
